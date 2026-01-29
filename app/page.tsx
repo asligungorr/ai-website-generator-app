3c5e789ad4a3f9e4cf72ca731f7be04d36d603b1
Aslı Güngör <116109377+asligungorr@users.noreply.github.com>
2026-01-29 14:08:21 +0300

    Initialize Next.js app and setup shadcn/ui

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Header from "./_components/Header";
+
+export default function Home() {
+  return (
+    <div> 
+      <Header/>
+    </div>
+  );
+}
